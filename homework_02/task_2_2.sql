6d51a6c0140f35d31ef0266ec13384e5d98e7308
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-04-19 12:37:18 +0700

    change task_2_2

--- homework_02/task_2_2.sql
+++ homework_02/task_2_2.sql
@@ -2,7 +2,7 @@
 Создайте базу данных example, разместите в ней таблицу users, 
 состоящую из двух столбцов, числового id и строкового name
  */
-
+DROP DATABASE IF EXISTS example;
 CREATE DATABASE example;
 DROP TABLE IF EXISTS example.users;
 CREATE TABLE example.users (
